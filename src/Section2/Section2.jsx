2ea68b7114d9ee2f246942068915182b223b0ff6
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-08 23:37:24 +0530

    section2 content change

--- src/Section2/Section2.jsx
+++ src/Section2/Section2.jsx
@@ -8,25 +8,25 @@ import './Section2.css'
 const Section2 = () => {
   let arr=[
     {image:icon1,
-    title:'sdfghjnm,cvbnmghjkfgh  cvgbhnjmk',
-  content:'Lorem, ipsum dolor sit amet consectetur. Labore cum dicta fugit doloremque.'},
+    title:'  Online Banking',
+  content:'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.'},
     {image:icon2,
-    title:'sdfghjnm,cvbnmghjkfgh  cvgbhnjmk',
-  content:'Lorem, ipsum dolor sit amet consectetur. Labore cum dicta fugit doloremque.'},
+    title:'Simple Budgeting',
+  content:'  See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.'},
     {image:icon3,
-    title:'sdfghjnm,cvbnmghjkfgh  cvgbhnjmk',
-  content:'Lorem, ipsum dolor sit amet consectetur. Labore cum dicta fugit doloremque.'},
+    title:'Fast Onboarding',
+  content:' We don’t do branches. Open your account in minutes online and start taking control of your finances right away.'},
     {image:icon4,
-    title:'sdfghjnm,cvbnmghjkfgh  cvgbhnjmk',
-  content:'Lorem, ipsum dolor sit amet consectetur. Labore cum dicta fugit doloremque.'}
+    title:' Open API',
+  content:'  Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.'}
   ]
   return (
     <>
       <div className="section2-main">
         <div className="headding">
           <h3>Why choose Easybank?</h3>
-          <p>Lorem, ipsum dolor sit amet consectetur
-            . Labore cum dicta fugit doloremque.</p>
+          <p> We leverage Open Banking to turn your bank account into your financial hub. Control 
+  your finances like never before.</p>
         </div>
         <div className="boxes">
           {
